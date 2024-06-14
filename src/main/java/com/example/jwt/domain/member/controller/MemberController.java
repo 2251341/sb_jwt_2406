4518702a62a9001c08ba7c902d8a50760baa645a
2251341 <dydwksos246@naver.com>
2024-06-14 15:39:11 +0900

    api versioning

--- src/main/java/com/example/jwt/domain/member/controller/MemberController.java
+++ src/main/java/com/example/jwt/domain/member/controller/MemberController.java
@@ -14,8 +14,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
+
+
 @RestController
-@RequestMapping("/member")
+@RequestMapping(value = "api/v1/member", produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
@@ -53,3 +56,4 @@ public class MemberController {
         );
     }
 }
+
