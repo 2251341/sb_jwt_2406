08db4951369485a3046dd8d89219c2a30d3ff008
2251341 <dydwksos246@naver.com>
2024-06-18 15:37:05 +0900

    article post

--- src/main/java/com/example/jwt/domain/article/controller/ArticleController.java
+++ src/main/java/com/example/jwt/domain/article/controller/ArticleController.java
@@ -2,15 +2,20 @@ package com.example.jwt.domain.article.controller;
 
 import com.example.jwt.domain.article.entity.Article;
 import com.example.jwt.domain.article.service.ArticleService;
+import com.example.jwt.domain.member.entity.Member;
+import com.example.jwt.domain.member.service.MemberService;
 import com.example.jwt.global.reData.RsData;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,6 +24,8 @@ import java.util.List;
 @RequestMapping(value = "/api/v1/articles")
 public class ArticleController {
     private final ArticleService articleService;
+    private final MemberService memberService;
+
 
     @AllArgsConstructor
     @Getter
@@ -28,7 +35,7 @@ public class ArticleController {
 
     @GetMapping(value = "")
     @Operation(summary = "다건조회")
-    public RsData<ArticlesResponse> articles(){
+    public RsData<ArticlesResponse> articles() {
         List<Article> articles = articleService.findAll();
 
         return RsData.of(
@@ -46,7 +53,7 @@ public class ArticleController {
 
     @GetMapping(value = "/{id}")
     @Operation(summary = "단건조회")
-    public RsData<ArticleResponse> article(@PathVariable("id") Long id){
+    public RsData<ArticleResponse> article(@PathVariable("id") Long id) {
         return articleService.findById(id).map(article -> RsData.of(
                 "S-1",
                 "성공",
@@ -57,4 +64,37 @@ public class ArticleController {
                 null
         ));
     }
+
+    @Data
+    public static class WriteRequest {
+        @NotBlank
+        private String subject;
+        @NotBlank
+        private String content;
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class WriteResponse {
+        private final Article article;
+    }
+
+    @PostMapping(value = "")
+    @Operation(summary = "등록", security = @SecurityRequirement(name = "bearerAuth"))
+    public RsData<WriteResponse> write(
+            @AuthenticationPrincipal User user,
+            @Valid @RequestBody WriteRequest writeRequest
+    ) {
+        Member member = memberService.findByUsername(user.getUsername()).orElseThrow();
+        RsData<Article> writeRs = articleService.write(member, writeRequest.getSubject(), writeRequest.getContent());
+
+        if (writeRs.isFail()) return (RsData) writeRs;
+
+        return RsData.of(
+                writeRs.getResultCode(),
+                writeRs.getMsg(),
+                new WriteResponse(writeRs.getData())
+        );
+    }
+
 }
